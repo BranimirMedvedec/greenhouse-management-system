4f5b940ce50f32cdceed6de200f6caa3b0d775e0
Branimir Medvedec <94074954+BranimirMedvedec@users.noreply.github.com>
2024-06-04 11:00:36 +0200

    add home page with cards, add aceternity library

--- app/page.js
+++ app/page.js
@@ -1,10 +1,43 @@
-import { ModeToggle } from "@/components/mode-toggle"
-import Image from "next/image"
+"use client"
+import { HoverEffect } from "@/components/ui/card-hover-effect"
+
+export const greenhouses = [
+	{
+		title: "Plastenik 1",
+		description: "Plastenik 1 sadrzi paradajz",
+		link: "http://localhost:3000/plastenik1",
+	},
+	{
+		title: "Plastenik 2",
+		description: "Plastenik 2 sadrzi papriku",
+		link: "http://localhost:3000/plastenik2",
+	},
+	{
+		title: "Plastenik 3",
+		description: "Plastenik 3 sadrzi krastavac",
+		link: "http://localhost:3000/plastenik3",
+	},
+	{
+		title: "Plastenik 4",
+		description: "Plastenik 4 sadrzi krumpir",
+		link: "http://localhost:3000/plastenik4",
+	},
+	{
+		title: "Plastenik 5",
+		description: "Plastenik 5 sadrzi luk",
+		link: "http://localhost:3000/plastenik5",
+	},
+	{
+		title: "Plastenik 6",
+		description: "Plastenik 6 sadrzi mrkvu",
+		link: "http://localhost:3000/plastenik6",
+	},
+]
 
 export default function Home() {
 	return (
-		<div>
-			<p>Home page</p>
+		<div className="max-w-7xl mx-auto px-8">
+			<HoverEffect items={greenhouses} />
 		</div>
 	)
 }
--- components/nav-layout.jsx
+++ components/nav-layout.jsx
@@ -231,9 +231,7 @@ export function NavLayout({ children }) {
 						</DropdownMenuContent>
 					</DropdownMenu>
 				</header>
-				<main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-					{children}
-				</main>
+				<main>{children}</main>
 			</div>
 		</div>
 	)
