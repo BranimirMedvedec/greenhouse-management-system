f069e1b6a23a74f65ee5bca743717ff90fcea07c
esembera <ernest@bykonstrukt.com>
2024-06-09 13:54:28 +0200

    add .env to gitignore and cleanup code

--- app/page.js
+++ app/page.js
@@ -1,45 +1,40 @@
 "use client"
 import { HoverEffect } from "@/components/ui/card-hover-effect"
-import { usePathname } from "next/navigation"
-
-const isLocal = process.env.NODE_ENV === "development";
-const baseUrl = isLocal ? "http://localhost:3000" : "https://greenhouse-management-system.vercel.app";
 
 export const greenhouses = [
     {
         title: "Plastenik 1",
         description: "Plastenik 1 sadrzi paradajz",
-        link: `${baseUrl}/plastenik/1`,
+        link: `/plastenik/1`,
     },
     {
         title: "Plastenik 2",
         description: "Plastenik 2 sadrzi papriku",
-        link: `${baseUrl}/plastenik/2`,
+        link: `/plastenik/2`,
     },
     {
         title: "Plastenik 3",
         description: "Plastenik 3 sadrzi krastavac",
-        link: `${baseUrl}/plastenik/3`,
+        link: `/plastenik/3`,
     },
     {
         title: "Plastenik 4",
         description: "Plastenik 4 sadrzi krumpir",
-        link: `${baseUrl}/plastenik/4`,
+        link: `/plastenik/4`,
     },
     {
         title: "Plastenik 5",
         description: "Plastenik 5 sadrzi luk",
-        link: `${baseUrl}/plastenik/5`,
+        link: `/plastenik/5`,
     },
     {
         title: "Plastenik 6",
         description: "Plastenik 6 sadrzi mrkvu",
-        link: `${baseUrl}/plastenik/6`,
+        link: `/plastenik/6`,
     },
 ]
 
 export default function Home() {
-    const pathname = usePathname();
 
     return (
         <div className="max-w-7xl mx-auto px-8">
